207a8448a9423325b92b7ea3e1ca56c924f381b1
Nir Oren <>
2020-09-30 16:53:26 +0100

    updated examples to make them slightly more readable

--- questions/multiple_choice.py
+++ questions/multiple_choice.py
@@ -11,11 +11,13 @@ qs='''Which of the following is true?
 '''
 
 def make_options(dict):
+'''Note in the following we first set the correct answer as a static string, create random strings, and shuffle them around.'''
   o=['1+2=3',f"{random.randint(0,5)}+{random.randint(0,5)}={random.randint(11,15)}","The rain in spain stays mainly in the plain"]
   random.shuffle(o)
   dict["o"]=o
 
 def correct_answer(dict,answers):
+  '''Just find the correct answer'''
   return dict["o"].index('1+2=3')==int(answers["a"])-1
 
 qv=[make_options]
